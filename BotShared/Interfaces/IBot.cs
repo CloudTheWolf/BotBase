9a318a60a5581ac1d53e939b203f6cbe29537d6f
Michael Howard <ukkmmichael@gmail.com>
2020-06-29 12:23:03 +0100

    Interactivity added to IBot Interface

--- BotShared/Interfaces/IBot.cs
+++ BotShared/Interfaces/IBot.cs
@@ -1,9 +1,11 @@
 ﻿using DSharpPlus.CommandsNext;
-
+using DSharpPlus.Interactivity;
 namespace BotShared.Interfaces
 {
     public interface IBot
     {
         CommandsNextExtension Commands { get; set; }
+        InteractivityExtension Interactivity { get; set; }
+        
     }
 }
