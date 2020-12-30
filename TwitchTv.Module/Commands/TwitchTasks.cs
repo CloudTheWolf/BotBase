99601fd03605cfbd357aa9229d1a5fc63727f3fa
Michael Howard <mhoward@CloudTheWolf.com>
2020-12-30 15:09:28 +0000

    Update TwitchTasks.cs
    Added Missing Logging Output

--- TwitchTv.Module/Commands/TwitchTasks.cs
+++ TwitchTv.Module/Commands/TwitchTasks.cs
@@ -85,9 +85,11 @@ namespace TwitchTv.Module.Commands
                     {
                         try
                         {
+                            
                             var discordId = ulong.Parse(stream["discordId"].ToString());
                             var guild = client.GetGuildAsync(ulong.Parse(stream["guildId"].ToString()),
                                 false).Result;
+                            Ttv.Logger.LogInformation($"Checking {stream["name"]} in {guild.Name}");
                             if (!IsMemberStillHere(discordId, client,guild)) continue;
                             var streamId = twitch.GetChannelId(stream["name"].ToString());
                             if (!string.IsNullOrEmpty(streamId) || !twitch.IsOnline(streamId)) continue;
