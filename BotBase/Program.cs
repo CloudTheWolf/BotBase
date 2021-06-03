3e1a3ee1e4b5a2f642846fd25dc6fdd2d42bb217
Michael Howard <mhoward@CloudTheWolf.com>
2021-06-03 15:48:02 +0100

    Update Program.cs

--- BotBase/Program.cs
+++ BotBase/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace BotBase
-{ 
+{
     public class Program
     {
         public static void Main(string[] args)
